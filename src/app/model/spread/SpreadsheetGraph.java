5359d5921aafd9e4ffe4959177a38c5676b22763
Jace Hamblin <jacehamblin@live.com>
2025-06-02 19:36:34 -0700

    Added javadocs

--- src/app/model/spread/SpreadsheetGraph.java
+++ src/app/model/spread/SpreadsheetGraph.java
@@ -3,8 +3,22 @@ package app.model.spread;
 import app.model.expr.ExpressionReader;
 import app.model.expr.GrammarExpressionReader;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Queue;
+import java.util.List;
+import java.util.ArrayDeque;
+import java.util.Iterator;
 
+
+/**
+ * Spreadsheet ADT that holds all data about cells and their relation to other cells in
+ * a graph, implemented as an adjacency list.
+ *
+ * @author Jace Hamblin
+ * @author Roman Bureacov
+ */
 public class SpreadsheetGraph implements Spreadsheet {
     private final int size;
     private int rows;
@@ -13,6 +27,11 @@ public class SpreadsheetGraph implements Spreadsheet {
     private boolean cycle;
     private final ExpressionReader mainReader;
 
+    /**
+     * Constructor for the spreadsheet with the given number of rows and columns
+     * @param theRowNum The number of rows to be in the spreadsheet.
+     * @param theColumnNum The number of columns to be in the spreadsheet.
+     */
     public SpreadsheetGraph(int theRowNum, int theColumnNum) {
         mainReader = new GrammarExpressionReader();
         rows = theRowNum;
@@ -21,6 +40,11 @@ public class SpreadsheetGraph implements Spreadsheet {
         adjList = new HashMap<>();
     }
 
+    /**
+     * Gets the cell value at the given row and column
+     * @param theRowColumn the row and column of the desired cell, as a string, in the format "R#C#"
+     * @return The value of the cell.
+     */
     @Override
     public Double getCellValue(String theRowColumn) {
         if (adjList.containsKey(theRowColumn)) {
@@ -30,6 +54,11 @@ public class SpreadsheetGraph implements Spreadsheet {
         }
     }
 
+    /**
+     * Gets the cell instructions at the given row and column in string format.
+     * @param theRowColumn the row and column of the desired cell, as a string, in the format "R#C#"
+     * @return The cells instructions.
+     */
     @Override
     public String getCellInstructions(String theRowColumn) {
         theRowColumn = theRowColumn.trim();
@@ -45,11 +74,23 @@ public class SpreadsheetGraph implements Spreadsheet {
         return returnInstructions;
     }
 
+    /**
+     * Gets the cell instructions at the given row and column in int format.
+     * @param theRow the row to look at
+     * @param theColumn the column to look at
+     * @return The cell instructions.
+     */
     @Override
     public String getCellInstructions(final int theRow, final int theColumn) {
         return this.getCellInstructions(this.toCellRef(theRow, theColumn));
     }
 
+    /**
+     * Sets cell instructions and evaluates cell values with provided instructions, cell is found using string row and column
+     * @param theInstructions the instructions to be provided to the cell, must begin with "=" to be evaluated as an expression,
+     *                        otherwise will attempt to evaluate as a literal. If not a literal and no "=", will evaluate as 0.
+     * @param theRowColumn the row and column of the desired cell, as a string, in the format "R#C#"
+     */
     @Override
     public void setCellInstructions(String theInstructions, String theRowColumn) {
         theInstructions = theInstructions.trim();
@@ -76,41 +117,76 @@ public class SpreadsheetGraph implements Spreadsheet {
         }
     }
 
+    /**
+     * Sets cell instructions and evaluates cell values with provided instructions, cell is found using int row and column.
+     * @param theInstructions the instruction to be provided to the cell, must begin with "=" to be evaluated
+     *                        as an expression, otherwise will attempt to evaluate as a literal. If not a literal
+     *                        and no "=", it will evaluate as 0.
+     * @param theRow the row to insert the instruction
+     * @param theColumn the column to insert the instruction
+     */
     @Override
     public void setCellInstructions(final String theInstructions, final int theRow, final int theColumn) {
         this.setCellInstructions(theInstructions, this.toCellRef(theRow, theColumn));
     }
 
+    /**
+     * Gets the number of rows in the spreadsheet.
+     * @return the number of rows.
+     */
     @Override
     public int getRowCount() {
         return rows;
     }
 
+    /**
+     * Gets the number of columns in the spreadsheet.
+     * @return The number of columns
+     */
     @Override
     public int getColumnCount() {
         return columns;
     }
 
+    /**
+     * Sets the number of rows in the spreadsheet.
+     * @param theCount the number of rows to resize to
+     */
     @Override
     public void setRowCount(final int theCount) {
         rows = theCount;
     }
 
+    /**
+     * Sets the number of columns in the spreadsheet.
+     * @param theCount the number of columns to resize to
+     */
     @Override
     public void setColumnCount(final int theCount) {
         columns = theCount;
     }
 
+    /**
+     * Gets the size of the spreadsheet by number of cells.
+     * @return the size of the spreadsheet in number of cells.
+     */
     @Override
     public int size() {
         return size;
     }
 
+    /**
+     * Formats two integers into a CellRef of the format "R#C#".
+     * @param pRow the row of interest.
+     * @param pCol the column of interest.
+     * @return the CellRef string representation of the parameters in the format "R#C#".
+     */
     @Override
     public String toCellRef(final int pRow, final int pCol) {
         return "R" + (pRow + 1) + "C" + (pCol + 1);
     }
 
+    //Method to evaluate each cells function and set its value to the result
     private void evaluateInstructions() {
         Queue<GraphVertex> ordering = topSort();
         //Cycle checker
@@ -157,6 +233,7 @@ public class SpreadsheetGraph implements Spreadsheet {
         }
     }
 
+    //Helper method that performs topological sort on the spreadsheet and returns a queue with the order to evaluate
     private Queue<GraphVertex> topSort() {
         setIndegree();
         Queue<GraphVertex> ordering = new ArrayDeque<>();
@@ -174,6 +251,7 @@ public class SpreadsheetGraph implements Spreadsheet {
         return ordering;
     }
 
+    //Helper method that finds all vertices with indegree equal to 0, and returns them in a queue
     private Queue<GraphVertex> noDegreeQueue() {
         Collection<GraphVertex> vertices = adjList.values();
         Iterator<GraphVertex> verIterator = vertices.iterator();
@@ -187,6 +265,8 @@ public class SpreadsheetGraph implements Spreadsheet {
         return noDegree;
     }
 
+    //Helper method that sets the indegree of each vertex according to how many cell references are in the cell function
+    //Automatically sets edges for these references
     private void setIndegree() {
         Collection<GraphVertex> vertices = adjList.values();
         for (GraphVertex tempVertex : vertices) {
